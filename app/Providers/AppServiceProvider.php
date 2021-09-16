3b04157e637b73473b2158652d4d3ac546d21647
Solomon Eseme <kaperskyguru@gmail.com>
2021-09-16 10:27:06 +0100

    feat: force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        dd($this->app->environment());
         if ($this->app->environment('production')) {
+
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
